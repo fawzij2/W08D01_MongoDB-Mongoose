12f727b3dc407a01fc59374d66e4133e560206d7
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-17 00:19:54 +0300

    practice 1

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ const app = express();
 app.use(express.json());
 
 app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+app.post("/create/todo", (req, res) => {
+    const task = req.body.task;
+    const description = req.body.description;
+    const deadline = req.body.deadline;
+    console.log(deadline); 
+    const isCompleted = req.body.isCompleted;
+    const priority = req.body.priority;
+    const todo = new todoModel({
+        task: task , 
+        description: description,
+        deadline: deadline,
+        isCompleted: isCompleted,
+        priority: priority
+    });
+
+
+    todo.save()
+    .then((result)=>{
+        res.json(result);
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+});
 app.put("/update/todo", (req, res) => {});
 app.delete("/delete/todo", (req, res) => {});
 
