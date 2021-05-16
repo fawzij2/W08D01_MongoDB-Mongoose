2ff785bd3e4230285a05d2ad3604a06af5101936
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-16 23:30:56 +0300

    pulse check 3 & 4

--- schema.js
+++ schema.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const todo_list = new mongoose.Schema({
+    task: String,
+    description: String,
+    deadline: Date,
+    isCompleted: Boolean,
+    priority: Number
+});
+
+module.exports = mongoose.model("Todo", todo_list);
