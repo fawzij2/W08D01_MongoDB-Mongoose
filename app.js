bd5822ba6243c32e4538aab0a7edb719e39d5694
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-17 00:35:56 +0300

    practice 4

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ const db = require("./db");
 const app = express();
 app.use(express.json());
 
-app.get("/todos", (req, res) => {});
+//practice 3
+app.get("/todos", (req, res) => {
+    todoModel.find({})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+});
+
+//practice4
+app.get("/todos/completed", (req, res) => {
+    todoModel.find({isCompleted:true})
+    .then((result)=>{
+        res.json(result);
+    })
+    .catch((err)=>{
+        res.json(err);
+    })
+});
+
+// practice 1
 app.post("/create/todo", (req, res) => {
     const task = req.body.task;
     const description = req.body.description;
